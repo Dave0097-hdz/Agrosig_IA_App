6941bca0c7812d948d2ac7ce87dac8b1b840c051
davidch <david087hdz@gmail.com>
2025-11-30 13:30:14 -0600

    First commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.agrosig_app"
+    compileSdk = 36
+    ndkVersion = "29.0.14206865"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    configurations.all {
+        resolutionStrategy {
+            force("com.google.firebase:firebase-messaging:24.1.0")
+            exclude(group = "com.google.firebase", module = "firebase-iid")
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.example.agrosig_app"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-messaging-ktx")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+}
---
+++ app/src/main/kotlin/com/example/agrosig_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.agrosig_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
